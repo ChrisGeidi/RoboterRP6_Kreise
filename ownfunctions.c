a402796261ed46a46a0d48b0c1387860d10076e5
Christoph <christoph.geitner@gmail.com>
2017-06-14 12:10:11 +0200

    added files for own functions

---
+++ ownfunctions.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *
+ * Project: Kreisfahren mit dem RP6
+ * File:    ownfunctions.c
+ *
+ * Ingenieurorientierte Programmierung
+ * Dr. Alexander Kling
+ *
+ * Carsten Bevermann
+ * Christoph Geitner
+ * Jens Reimer
+ * Marc Töpker
+ *
+ *****************************************************************************/
+
+
+
+ /***************** End of file <ownfunctions.c> *****************************/
---
+++ ownfunctions.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *
+ * Project: Kreisfahren mit dem RP6
+ * File:    ownfunctions.h
+ *
+ * Ingenieurorientierte Programmierung
+ * Dr. Alexander Kling
+ *
+ * Carsten Bevermann
+ * Christoph Geitner
+ * Jens Reimer
+ * Marc Töpker
+ *
+ *****************************************************************************/
+
+
+/***************** End of file <ownfunctions.h> *****************************/
