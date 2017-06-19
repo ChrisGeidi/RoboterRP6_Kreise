73a54fbe3d586db6f44d4d581c484f9b7724e69b
jereimer <jens@reimer-warendorf.de>
2017-06-19 11:02:17 +0200

    Für Marc
    Anpassung Funktion
    Enum

--- main.c
+++ main.c
@@ -18,20 +18,27 @@
 #define HALBER_ACHSABSTAND  51.5
 #define PI                  3.142
 
+    enum direction
+    {
+        left,
+        right,
+        straight
+    };
+
     int iRadius     = 0; // Radius
     int iLeftSpeed;
     int iRightSpeed;
     int iSpeed = 60;
-    void calculateSpeed(int , int , char );
-    void driveRad (int,int,char);
+    void calculateSpeed(int, int, int);
+    void driveRadius (int,int,int direction);
 
-    uint16_t calculateDistanceForLeftTrack_Arc(int, char);
+    uint16_t calculateDistanceForLeftTrack_Arc(int, int direction);
 
 
 int main(void)
 {
     initRobotBase();
-    int r = 400;
+    int r = 200;
     mleft_dist = 0;
     mright_dist = 0;
     iLeftSpeed = 80;
@@ -42,11 +49,12 @@ int main(void)
     powerON(); // Encoder und Motorstromsensoren anschalten!
 
     moveAtSpeed(iLeftSpeed,iRightSpeed); // Geschwindigkeit einstellen#
+    void driveRadius(r, speed, left);
 
     while(true)
     {
 
-    void driveRad(r, speed,'R');
+
 
         moveAtSpeed(iLeftSpeed,iRightSpeed);
 
@@ -65,7 +73,7 @@ int main(void)
     return 0;
 }
 
-void driveRad (int Radius, int speed, char direction)
+void driveRadius (int Radius, int speed, int direction)
 {
         if (getLeftDistance()== Radius)
         {
@@ -74,7 +82,7 @@ void driveRad (int Radius, int speed, char direction)
 
         if (getLeftDistance()== (Radius+calculateDistanceForLeftTrack_Arc(Radius  , direction)))
         {
-            calculateSpeed(Radius, speed, 'G');
+            calculateSpeed(Radius, speed, straight);
         }
         if (getLeftDistance()> (2*Radius+calculateDistanceForLeftTrack_Arc(Radius, direction)))
         {
@@ -84,39 +92,39 @@ void driveRad (int Radius, int speed, char direction)
         }
 };
 
-void calculateSpeed(int iRadius, int iSpeed, char direction)
+void calculateSpeed(int iRadius, int iSpeed, int direction)
 {
     switch(direction)
     {
-    case 'R':
+    case 1:
         iLeftSpeed  = iSpeed;
         iRightSpeed = iLeftSpeed / (iRadius + HALBER_ACHSABSTAND) * (iRadius - HALBER_ACHSABSTAND);
         break;
 
-    case 'L':
+    case 0:
         iRightSpeed = iSpeed;
         iLeftSpeed  = iRightSpeed / (iRadius + HALBER_ACHSABSTAND) * (iRadius - HALBER_ACHSABSTAND);
         break;
 
-    case 'G':
+    case 2:
         iLeftSpeed  = iSpeed;
         iRightSpeed = iSpeed;
         break;
     }
 }
 
-uint16_t calculateDistanceForLeftTrack_Arc(int iRadius, char direction)
+uint16_t calculateDistanceForLeftTrack_Arc(int iRadius, int direction)
 {
     uint16_t distance = 0;
 
     switch(direction)
     {
-    case 'R':
-        distance = 2 * PI * (iRadius + HALBER_ACHSABSTAND);
+    case 1:
+        distance = 2 * PI * (iRadius + HALBER_ACHSABSTAND)*3/4;
         break;
 
-    case 'L':
-        distance = 2 * PI * (iRadius - HALBER_ACHSABSTAND);
+    case 0:
+        distance = 2 * PI * (iRadius - HALBER_ACHSABSTAND)*3/4;
         break;
     }
 
